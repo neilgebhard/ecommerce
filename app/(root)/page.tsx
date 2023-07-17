1ae4cab157f665dd09ba2ea870b36b584f5430e0
Neil Gebhard <neilgebhard@gmail.com>
2023-07-17 15:57:42 +1000

    Install next, tailwind, and shadcn

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <main>
+      <p>Hello world!</p>
+      <Button>Button</Button>
+    </main>
+  )
+}
